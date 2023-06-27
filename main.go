ca29e06c9cbe464adcbb5cc3125b7141b15d51f7
Tseng Hsi-Huang <9501sam@gmail.com>
2023-06-27 21:27:30 +0800

    complete Hub.run()

--- main.go
+++ main.go
@@ -20,9 +20,14 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	hub := newHub()
-	go hub.run() // TODO
+	go hub.run()
 
 	http.HandleFunc("/", serveHome)
+
+	// http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	// 	serveWs(hub, w, r)
+	// })
+
 	err := http.ListenAndServe("127.0.0.1:8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
