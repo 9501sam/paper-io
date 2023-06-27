39d4803a2cf04036ff20338d820cdfee85d512de
Tseng Hsi-Huang <9501sam@gmail.com>
2023-06-27 22:05:21 +0800

    add serveWs()

--- client.go
+++ client.go
@@ -1,7 +1,35 @@
 package main
 
-type Client struct {
-	hub *Hub
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	hub  *Hub
+	conn *websocket.Conn
 	send chan []byte
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("enter serveWs") // TODO Observe This
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	// TODO
+	// go client.writePump()
+	// go client.readPump()
 }
